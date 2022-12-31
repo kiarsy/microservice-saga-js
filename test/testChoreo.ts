edcf3025f56d9cb4dcec5b77ada60144692f94c4
kiarsy <kiarsy@yahoo.com>
2022-12-31 16:55:59 +0400

    restructure project

--- test/testChoreo.ts
+++ test/testChoreo.ts
@@ -1,23 +1,17 @@
-import { SagaJS } from "../index";
 import { InMemoryCommunication } from "../Communication/inMemoryCommunication";
+import { SagaJS } from "../index";
+import { JsonTransformer } from "../Transformer/JsonTransformer";
 
 
-const sagaJs = new SagaJS(new InMemoryCommunication());
+// const sagaJs = new SagaJS(new InMemoryCommunication(new JsonTransformer()));
 
-sagaJs.onStep("stepA", (context) => {
-    console.log("StepA", context);
-    context.complete({ stepA: 'done' });
-});
+// sagaJs.onStep("stepA", (context) => {
+//     console.log("StepA", context);
+//     context.complete({ stepA: 'done' });
+// });
 
-sagaJs.onStep("stepB", (context) => {
-    console.log("stepB", context);
-});
+// sagaJs.onStep("stepB", (context) => {
+//     console.log("stepB", context);
+// });
 
-sagaJs.runChoreography("stepA", { start: 'here' });
-
-// todo: status : running,completed,in_compensation,failed
-// todo: retry
-// todo: timeout
-// todo: compensation
-// todo: choregraphy
-// todo: communicationTransportLayer : json | protoBuff
+// sagaJs.runChoreography("stepA", { start: 'here' });
